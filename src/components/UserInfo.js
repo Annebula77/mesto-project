baf3b0b446f8a3839eb13565fcf97d0c0428e87f
vartexxx <vartexxx29@yandex.ru>
2023-01-24 12:56:06 +0500

    feat: добавлен класс UserInfo

---
+++ src/components/UserInfo.js
@@ -0,0 +1,24 @@
+export default class UserInfo {
+    constructor(name, about, avatar) {
+        this._name = document.querySelector(name);
+        this._about = document.querySelector(about);
+        this._avatar = document.querySelector(avatar);
+    }
+    getUserInfo() {
+        this.name = this._name.textContent;
+        this.about = this._about.textContent;
+        this.avatar = this._avatar.src;
+        return this;
+    }
+    setUserInfo(data) {
+        if(data.name) {
+            this._name.textContent = data.name;
+        }
+        if(data.about) {
+            this._about.textContent = data.about;
+        }
+        if(data.avatar) {
+            this._avatar.src = data.avatar;
+        }
+    }
+}
